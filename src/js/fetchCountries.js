50a1b99512fd9876ee6cd5c1fe0eba6fa1e6df48
Abramov21 <104825295+Abramov21@users.noreply.github.com>
2022-08-18 16:38:53 +0300

    fix fetch

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,16 +1,13 @@
 function fetchCountries(search) {
-    return fetch(
-      `https://restcountries.com/v3.1/name/${search}?fields=name,capital,population,flags,languages`
-    )
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      });
-  }
+  return fetch(
+    `https://restcountries.com/v3.1/name/${search}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+}
 
-  export default fetchCountries;
+export default fetchCountries;
